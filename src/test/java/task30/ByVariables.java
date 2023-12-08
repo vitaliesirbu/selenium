6884d2a8a420c90540cedaed704126f2b8c8cb15
VitalieSirbu <vitalie.sirbu0@gmail.com>
2023-12-08 16:20:42 +0200

    Refactoring Task 30 v2

--- src/test/java/task30/ByVariables.java
+++ src/test/java/task30/ByVariables.java
@@ -3,11 +3,11 @@ package task30;
 import org.openqa.selenium.By;
 
 public class ByVariables {
-    public static By SignIn = By.xpath("/html/body/div[2]/header/div[1]/div/ul/li[2]/a");
-    public static By EmailInput = By.xpath("//*[@id=\"email\"]");
-    public static By PasswordInput = By.xpath("//*[@id=\"pass\"]");
-    public static By SignInButton = By.xpath("//*[@id=\"send2\"]/span");
+    public static By SignIn = By.className("authorization-link");
+    public static By EmailInput = new By.ById("email");
+    public static By PasswordInput = new By.ById("pass");
+    public static By SignInButton = new By.ById("send2");
 
-    public static By AssertElement = By.xpath("/html/body/div[2]/header/div[1]/div/ul/li[1]/span");
+    public static By AssertElement = By.className("logged-in");
 
 }
