51027751826aee4a781fecc256ec4c62cc0ea5e7
VitalieSirbu <vitalie.sirbu0@gmail.com>
2024-01-15 21:45:35 +0200

    Separating WaitUtils in different class.

--- src/main/java/com/coherentsolutions/training/automation/web/sirbu/pageobjects/HomePage.java
+++ src/main/java/com/coherentsolutions/training/automation/web/sirbu/pageobjects/HomePage.java
@@ -1,13 +1,22 @@
 package com.coherentsolutions.training.automation.web.sirbu.pageobjects;
 
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 
 public class HomePage {
+
+    private WebDriver driver;
+    private static final By LOGGED_IN_LOCATOR = By.className("logged-in");
 
     @FindBy(xpath = "//div[@class='panel header']//button[@type='button']")
     private WebElement myAccountButton;
@@ -18,11 +27,25 @@ public class HomePage {
     private WebElement welcomeMessage;
 
     public HomePage(WebDriver driver){
+        this.driver = driver;
         PageFactory.initElements(driver, this);
+    }
+
+    public boolean isLoggedIn() {
+        try {
+            return welcomeMessage.isDisplayed();
+        } catch (NoSuchElementException e) {
+            return false;
+        }
+    }
+
+    public static By getLoggedInLocator() {
+        return LOGGED_IN_LOCATOR;
     }
 
     public void logout(){
         myAccountButton.click();
+        new WebDriverWait(driver, Duration.ofSeconds(10)).until(ExpectedConditions.elementToBeClickable(logoutLink));
         logoutLink.click();
     }
     public String welcomeText(){
--- src/test/java/com/coherentsolutions/training/automation/web/sirbu/LoginLogoutTest.java
+++ src/test/java/com/coherentsolutions/training/automation/web/sirbu/LoginLogoutTest.java
@@ -3,16 +3,13 @@ package com.coherentsolutions.training.automation.web.sirbu;
 import com.coherentsolutions.training.automation.web.sirbu.pageobjects.HomePage;
 import com.coherentsolutions.training.automation.web.sirbu.pageobjects.LoginPage;
 import com.coherentsolutions.training.automation.web.sirbu.utilities.ConfigReader;
+import com.coherentsolutions.training.automation.web.sirbu.utilities.WaitUtils;
 import com.coherentsolutions.training.automation.web.sirbu.utilities.WebDriverSingleton;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
-import java.time.Duration;
 
 
 public class LoginLogoutTest {
@@ -20,36 +17,27 @@ public class LoginLogoutTest {
     private WebDriver driver;
     private LoginPage loginPage;
     private HomePage homePage;
-    private WebDriverWait wait;
     private String username;
     private String password;
     private String expectedResult;
+    private WaitUtils waitUtils;
 
     @BeforeClass
     public void setUp(){
-
         driver = WebDriverSingleton.getDriver();
         driver.get(ConfigReader.getInstance().getUrl());
         loginPage = new LoginPage(driver);
         homePage = new HomePage(driver);
-
-
         username = ConfigReader.getInstance().getUsername();
         password = ConfigReader.getInstance().getPassword();
         expectedResult = ConfigReader.getInstance().getExpectedResult();
-
-
-        loginPage = new LoginPage(driver);
-        homePage = new HomePage(driver);
-
-        wait = new WebDriverWait(driver,  Duration.ofSeconds(5));
+        waitUtils = new WaitUtils(driver);;
     }
 
     @Test
     public void testSuccessfulLogin() throws InterruptedException {
         loginPage.login(username,password);
-        Thread.sleep(5000);
-        wait.until(ExpectedConditions.presenceOfElementLocated(By.className("logged-in")));
+        waitUtils.waitForElementPresence(homePage.getLoggedInLocator(), 5);
         String loggedInText = homePage.welcomeText();
         Assert.assertEquals(loggedInText, expectedResult, "The actual result does not match the expected result.");
         System.out.println("Actual result matches expected result: " + loggedInText);
@@ -58,16 +46,9 @@ public class LoginLogoutTest {
     @Test(dependsOnMethods = {"testSuccessfulLogin"})
     public void testSuccessfulLogout(){
         homePage.logout();
-        Assert.assertTrue(isElementPresent(By.className("logged-in")), "Element is not present on the page.");
-    }
+        waitUtils.waitForElementAbsence(HomePage.getLoggedInLocator(), 5);
+        Assert.assertFalse(homePage.isLoggedIn(), "User is still logged in when they should be logged out.");
 
-    private boolean isElementPresent(By locator) {
-        try {
-            driver.findElement(locator);
-            return true;
-        } catch (org.openqa.selenium.NoSuchElementException e) {
-            return false;
-        }
     }
 
     @AfterClass
