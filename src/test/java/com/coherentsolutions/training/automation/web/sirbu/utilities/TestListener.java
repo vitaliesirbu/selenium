376364d586652f0b8675e775e8edf0ae99f1c135
VitalieSirbu <vitalie.sirbu0@gmail.com>
2024-02-06 15:20:06 +0200

    IMmplementing additioanl information for failed tests

--- src/test/java/com/coherentsolutions/training/automation/web/sirbu/utilities/TestListener.java
+++ src/test/java/com/coherentsolutions/training/automation/web/sirbu/utilities/TestListener.java
@@ -1,7 +1,12 @@
 package com.coherentsolutions.training.automation.web.sirbu.utilities;
 
 import com.coherentsolutions.training.automation.web.sirbu.BaseTest;
+import io.qameta.allure.Attachment;
+import org.openqa.selenium.Capabilities;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
 
@@ -10,12 +15,28 @@ public class TestListener implements ITestListener {
 
     @Override
     public void onTestFailure(ITestResult result) {
-        Object currentClass = result.getInstance();
-        WebDriver webDriver = ((BaseTest) currentClass).driver;
+        Object testClass = result.getInstance();
+        WebDriver driver = ((BaseTest) testClass).driver;
 
-        String testName = result.getName();
-        String filePath = "target/artifacts/screenshots/" + testName + "_" + System.currentTimeMillis() + ".png";
+        if (driver != null) {
 
-        ScreenshotUtil.captureAndSaveScreenshot(webDriver, filePath);
+            byte[] screenshotBytes = ((TakesScreenshot)driver).getScreenshotAs(OutputType.BYTES);
+            saveScreenshot(screenshotBytes);
+
+
+            Capabilities caps = ((RemoteWebDriver) driver).getCapabilities();
+            String browserInfo = String.format("%s Version: %s", caps.getBrowserName(), caps.getBrowserVersion());
+            attachBrowserInfo(browserInfo);
+        }
+    }
+
+    @Attachment(value = "Screenshot on failure", type = "image/png")
+    public byte[] saveScreenshot(byte[] screenshotBytes) {
+        return screenshotBytes;
+    }
+
+    @Attachment(value = "Browser Information", type = "text/plain")
+    public String attachBrowserInfo(String browserInfo) {
+        return browserInfo;
     }
 }
