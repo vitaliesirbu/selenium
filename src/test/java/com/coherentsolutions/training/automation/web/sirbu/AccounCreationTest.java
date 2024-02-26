a350be2198e6214c30db00f8d004ac56326ef010
VitalieSirbu <vitalie.sirbu0@gmail.com>
2024-02-26 14:03:33 +0200

    some minro changes.

--- src/test/java/com/coherentsolutions/training/automation/web/sirbu/AccounCreationTest.java
+++ src/test/java/com/coherentsolutions/training/automation/web/sirbu/AccounCreationTest.java
@@ -1,7 +1,6 @@
 package com.coherentsolutions.training.automation.web.sirbu;
 
 import com.coherentsolutions.training.automation.web.sirbu.pageobjects.AccountCreationPage;
-import com.coherentsolutions.training.automation.web.sirbu.pageobjects.HomePage;
 import com.coherentsolutions.training.automation.web.sirbu.pageobjects.LoginPage;
 import com.coherentsolutions.training.automation.web.sirbu.pageobjects.MyAccountPage;
 import com.coherentsolutions.training.automation.web.sirbu.utilities.ConfigReader;
@@ -32,7 +31,7 @@ public class AccounCreationTest extends BaseTest{
     private String password = configReader.getProperty("newPassword");
     private String expectedContactName = configReader.getProperty("expectedContactName");
 
-    @BeforeMethod
+    @BeforeClass
     @Override
     @Parameters({"platform", "browserName", "browserVersion"})
     public void setUp(@Optional String platform, @Optional String browserName, @Optional String browserVersion) throws MalformedURLException {
@@ -40,11 +39,6 @@ public class AccounCreationTest extends BaseTest{
         loginPage = new LoginPage(driver);
         accountCreationPage = new AccountCreationPage(driver);
         myAccountPage = new MyAccountPage(driver);
-    }
-
-    protected void registerNewAccount(String firstName, String lastName, String password, String email)
-    {
-        accountCreationPage.createAccount(firstName,lastName,password,email);
     }
 
     @Test(groups = {"loginGroup"}, description = "This test verifies new account registration functionality.")
@@ -62,7 +56,7 @@ public class AccounCreationTest extends BaseTest{
         WebElement contactInformation = driver.findElement(By.cssSelector(".box-content p"));
 
         String actualContactName = contactInformation.getText();
-        actualContactName = actualContactName.split("\n")[0];  // Assuming the name is on the first line.
+        actualContactName = actualContactName.split("\n")[0];
 
         Assert.assertEquals(actualContactName, expectedContactName, "The contact name does not match the expected value.");
 
